bedb82fb4b9e8241882f8a3ab9097b2652663b91
Steffen Winde <inserv@winde-ganzig.de>
2022-01-29 18:58:20 +0100

    edit Version

--- metadata.php
+++ metadata.php
@@ -17,74 +17,35 @@ $sMetadataVersion = '2.1';
  * Module information
  */
 $aModule = [
-    'id'          => 'swinde/swlagerampel',
-    'title'       => [
-        'de' => '.BEES - Lagerampel für Listenansichten',
-        'en' => '.BEES - Lagerampel für Listenansichten',
-    ],
-    'description' => [
-        'en' => 'Lagerampel für Listenansichten (V6).',
-        'de' => 'Lagerampel für Listenansichten (V6).',
-    ],
-    'thumbnail'   => '',
-    'version'     => '1.0',
-    'author'      => 'Steffen Winde',
-    'url'         => 'https://github.com/swinde/swlagerampel',
-    'email'       => '',
-    'extend'      => [],
-    'controllers' => [],
-    'templates'   => [],
-    'blocks'      => [
-        [
-            'template' => 'widget/product/listitem_line.tpl',
-            'block'    => 'widget_product_listitem_line_description',
-            'file'=>'/views/blocks/ampel_line.tpl'
-        ],
-        [
-            'template' => 'widget/product/listitem_infogrid.tpl',
-            'block'=>'widget_product_listitem_infogrid_titlebox',
-            'file'=>'/views/blocks/ampel_infogrid.tpl'
-        ],
-        [
-            'template' => 'widget/product/listitem_grid.tpl',
-            'block'=>'widget_product_listitem_infogrid_titlebox',
-            'file'=>'/views/blocks/ampel_grid.tpl'
-        ],
-        [
-            'theme' => 'dd_roxive',
-            'template' => 'widget/product/listitem_line.tpl',
-            'block'    =>  'widget_product_listitem_line_description',
-            'file'=>'/views/blocks/ampel_listitem_line_ForRoxive.tpl'
-        ],
-        [
-            'theme' => 'dd_roxive',
-            'template' => 'widget/product/listitem_infogrid.tpl',
-            'block'    =>  'widget_product_listitem_infogrid_titlebox',
-            'file'=>'/views/blocks/ampel_infogrid_ForRoxive.tpl'
-        ],
-        [
-            'theme' => 'dd_roxive',
-            'template' => 'widget/product/listitem_grid.tpl',
-            'block'    => 'widget_product_listitem_infogrid_titlebox',
-            'file'=>'/views/blocks/ampel_grid_ForRoxive.tpl'
-        ],
-		[
-			'theme' => 'moga',
-			'template' => 'widget/product/listitem_line.tpl',
-			'block'    =>  'widget_product_listitem_line_description',
-			'file'=>'/views/blocks/ampel_listitem_line_ForMoga.tpl'
-		],
-		[
-			'theme' => 'moga',
-			'template' => 'widget/product/listitem_infogrid.tpl',
-			'block'    =>  'widget_product_listitem_infogrid_titlebox',
-			'file'=>'/views/blocks/ampel_infogrid_ForMoga.tpl'
-		],
-		[
-			'theme' => 'moga',
-			'template' => 'widget/product/listitem_grid.tpl',
-			'block'    => 'widget_product_listitem_infogrid_titlebox',
-			'file'=>'/views/blocks/ampel_grid_ForMoga.tpl'
-		]
-    ],
+	'id'          => 'swlagerampel',
+	'title'       => [
+		'de' => '.BEES - Lagerampel für Listenansichten',
+		'en' => '.BEES - Lagerampel für Listenansichten',
+	],
+	'description' => [
+		'en' => 'Lagerampel für Listenansichten (V6).',
+		'de' => 'Lagerampel für Listenansichten (V6).',
+	],
+	'thumbnail'   => '',
+	'version'     => '1.3.1',
+	'author'      => 'Steffen Winde',
+	'url'         => 'https://github.com/swinde/swlagerampel',
+	'email'       => 'inser@winde-ganzig.de',
+	'extend'      => [],
+	'controllers' => [],
+	'templates'   => [],
+	'blocks'      => [
+		//Line-List
+		['template'=>'widget/product/listitem_line.tpl','block'=>'widget_product_listitem_line_description','file'=>'/views/blocks/ampel_line.tpl'],
+		['theme' => 'dd_roxive','template' => 'widget/product/listitem_line.tpl','block'=>'widget_product_listitem_line_description','file'=>'/views/blocks/ampel_listitem_line_ForRoxive.tpl'],
+		['theme' => 'moga','template' => 'widget/product/listitem_line.tpl','block'=>'widget_product_listitem_line_description','file'=>'/views/blocks/ampel_listitem_line_ForMoga.tpl'],
+		//Item-Grid_List
+		['template' => 'widget/product/listitem_grid.tpl','block'=>'widget_product_listitem_infogrid_titlebox','file'=>'/views/blocks/ampel_grid.tpl'],
+		['theme' => 'dd_roxive','template'=>'widget/product/listitem_grid.tpl','block'=>'widget_product_listitem_infogrid_titlebox','file'=>'/views/blocks/ampel_grid_ForRoxive.tpl'],
+		['theme' => 'moga','template' => 'widget/product/listitem_grid.tpl','block'=>'widget_product_listitem_grid_titlebox','file'=>'/views/blocks/ampel_infogrid_ForMoga.tpl'],
+
+		['template' => 'widget/product/listitem_infogrid.tpl','block'=>'widget_product_listitem_infogrid_titlebox','file'=>'/views/blocks/ampel_infogrid.tpl'],
+		['theme' => 'dd_roxive','template' => 'widget/product/listitem_infogrid.tpl','block'=>'widget_product_listitem_infogrid_titlebox','file'=>'/views/blocks/ampel_infogrid_ForRoxive.tpl'],
+		['theme' => 'moga','template' => 'widget/product/listitem_infogrid.tpl','block'=>'widget_product_listitem_infogrid_titlebox','file'=>'/views/blocks/ampel_infogrid_ForMoga.tpl'],
+	]
 ];
